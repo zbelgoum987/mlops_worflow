0a1f451d9ea21ac44dc1a5f1cb2fb23400ed36a1
zakariabelgoum <zakariabelgoum94@gmail.com>
2023-10-23 10:58:06 -0400

    add script for training

--- src/model/train.py
+++ src/model/train.py
@@ -1,74 +1,45 @@
-# Import libraries
-
-import argparse
-import glob
-import os
-
 import pandas as pd
-
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
+import os
+import argparse
+
+def read_data(path):
+    df = pd.read_csv(path)
+    return df
 
 
-# define functions
-def main(args):
-    # TO DO: enable autologging
+def preprocess_data(df):
+    X,y = df[df.columns[1:-1]].values, df[df.columns[-1]].values
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+    return X_train, X_test, y_train, y_test
 
-
-    # read data
-    df = get_csvs_df(args.training_data)
-
-    # split data
-    X_train, X_test, y_train, y_test = split_data(df)
-
-    # train model
-    train_model(args.reg_rate, X_train, X_test, y_train, y_test)
-
-
-def get_csvs_df(path):
-    if not os.path.exists(path):
-        raise RuntimeError(f"Cannot use non-existent path provided: {path}")
-    csv_files = glob.glob(f"{path}/*.csv")
-    if not csv_files:
-        raise RuntimeError(f"No CSV files found in provided data path: {path}")
-    return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
-
-
-# TO DO: add function to split data
-
-
-def train_model(reg_rate, X_train, X_test, y_train, y_test):
-    # train model
-    LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
-
+def train_and_test_model(X_train, X_test, y_train, y_test):
+    model = LogisticRegression(random_state=0).fit(X_train, y_train)
+    train_acc = model.score(X_train, y_train)
+    print(f"trianing accuracy: {train_acc}")
+    test_acc = model.score(X_test, y_test)
+    print(f"testing accuracy: {test_acc}")
 
 def parse_args():
-    # setup arg parser
     parser = argparse.ArgumentParser()
-
-    # add arguments
-    parser.add_argument("--training_data", dest='training_data',
-                        type=str)
-    parser.add_argument("--reg_rate", dest='reg_rate',
-                        type=float, default=0.01)
-
-    # parse args
+    parser.add_argument("--training_data", dest='training_data',type=str)
     args = parser.parse_args()
-
-    # return args
     return args
 
-# run script
+def main(args):
+    df = read_data(args.training_data)
+    X_train, X_test, y_train, y_test = preprocess_data(df)
+    train_and_test_model(X_train, X_test, y_train, y_test)
+
+
 if __name__ == "__main__":
-    # add space in logs
-    print("\n\n")
-    print("*" * 60)
-
-    # parse args
     args = parse_args()
-
-    # run main function
     main(args)
 
-    # add space in logs
-    print("*" * 60)
-    print("\n\n")
+
+
