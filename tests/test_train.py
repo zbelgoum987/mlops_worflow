94b1c029f43e9da556c165de87f4837ae43c43fe
zbelgoum987 <zbelgoum987@gmail.com>
2023-10-24 15:46:56 +0000

    minor changes

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,22 +1,22 @@
-from model.train import get_csvs_df
+from model.train import read_data
 import os
 import pytest
 
 
 def test_csvs_no_files():
     with pytest.raises(RuntimeError) as error:
-        get_csvs_df("./")
+        read_data("./")
     assert error.match("No CSV files found in provided data")
 
 
 def test_csvs_no_files_invalid_path():
     with pytest.raises(RuntimeError) as error:
-        get_csvs_df("/invalid/path/does/not/exist/")
+        read_data("/invalid/path/does/not/exist/")
     assert error.match("Cannot use non-existent path provided")
 
 
 def test_csvs_creates_dataframe():
     current_directory = os.path.dirname(os.path.abspath(__file__))
     datasets_directory = os.path.join(current_directory, 'datasets')
-    result = get_csvs_df(datasets_directory)
+    result = read_data(datasets_directory)
     assert len(result) == 20
